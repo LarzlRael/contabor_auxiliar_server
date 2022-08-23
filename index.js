1aec505189cae9912fa19fa54be13d6f768d5084
Reynaldo G.T <rael.thasss@gmail.com>
2022-08-23 18:15:27 -0400

    Se separaron los archivos para un mejor control

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 const express = require('express')
 const app = express()
-const {
-  Builder,
-  Browser,
-  By,
-  Key,
-  until,
-  sendKeys,
-} = require('selenium-webdriver')
 
-var webdriver = require('selenium-webdriver')
-const chrome = require('selenium-webdriver/chrome')
-const chromedriver = require('chromedriver')
 
 /* chrome.setDefaultService(new chrome.ServiceBuilder(chromedriver.path).build()) */
 
@@ -22,38 +11,8 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 // habilitar la carpeta publica para ser accedida
-app.get('/', (req, res) => {
-  res.send('Hola mundo')
-})
-app.get('/revisarNit/:nit', async (req, res) => {
-  const { nit } = req.params
-  let driver = new webdriver.Builder().forBrowser('chrome').build()
 
-  try {
-    await driver.get(
-      'http://pbdw.impuestos.gob.bo:8080/gob.sin.padron.web/#/verificar/estadoContribuyente',
-    )
-    const input = await driver
-      .findElement(
-        By.xpath('//*[@id="frmLogin"]/fieldset/section/div/label[2]/input'),
-      )
-      .sendKeys(nit)
-    /* await driver.wait(until.titleIs('webdriver - Google Search'), 1000) */
-    const sendButton = await driver.findElement(
-      By.xpath('//*[@id="frmLogin"]/fieldset/div/button'),
-    )
-    console.log(sendButton)
-    await sendButton.click()
-    const res = await driver.findElement(By.xpath('//*[@id="toast-container"]'))
-    console.log(res)
-    /* setTimeout(async () => {}, 5000) */
-    /* res.send(await driver.getPageSource()) */
-    
-  } finally {
-    /* await driver.quit() */
-  }
-  /* return res.send(req.params.nit) */
-})
+app.use(require('./routes/routes'))
 
 app.listen(port, () => {
   console.log(`Entrar a: http://localhost:${port}`)

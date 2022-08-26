cb144eab065f55a9f9eef276b4f1e81b7bff1838
Reynaldo G.T <rael.thasss@gmail.com>
2022-08-25 23:04:39 -0400

    Se agregaron los demas campos

--- routes/routes.js
+++ routes/routes.js
@@ -8,12 +8,16 @@ router.get('/', (req, res) => {
 router.post('/dialogflow', async (req, res) => {
   console.log(req.body.texto)
   console.log(req.body.unidades)
-  const { texto, unidades } = req.body
+  const { unidades, cuenta, ingresos, currency, df, it } = req.body
   const io = req.app.get('socketio')
 
   io.emit('mensaje', {
-    texto,
     unidades,
+    cuenta,
+    ingresos,
+    currency,
+    df,
+    it,
   })
   res.json('se enviaron los datos')
 })

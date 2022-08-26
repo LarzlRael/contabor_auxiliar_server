66dd42cda86c0c3283eb152b41ae6c82832bfe99
Reynaldo G.T <rael.thasss@gmail.com>
2022-08-25 22:15:25 -0400

    que fue gente

--- routes/routes.js
+++ routes/routes.js
@@ -8,11 +8,12 @@ router.get('/', (req, res) => {
 router.post('/dialogflow', async (req, res) => {
   console.log(req.body.texto)
   console.log(req.body.unidades)
-  const { texto } = req.body
+  const { texto, unidades } = req.body
   const io = req.app.get('socketio')
 
   io.emit('mensaje', {
     texto,
+    unidades,
   })
   res.json('se enviaron los datos')
 })
